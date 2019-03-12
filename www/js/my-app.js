7e51443078685abbf857c789603e511ef3f152d2
Gaston Graciani <gastongraciani@gmail.com>
2019-03-12 18:20:27 -0300

    Changes about google auth

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -48,21 +48,36 @@ $$(document).on('page:init', function (e) {
   $$('#GoogleLogin').on('click', function (e) {
     console.log("Hola")
 
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
+    // firebase.auth().signInWithPopup(provider).then(function (result) {
+    //   // This gives you a Google Access Token. You can use it to access the Google API.
+    //   var token = result.credential.accessToken;
+    //   // The signed-in user info.
+    //   var user = result.user;
+    //   // ...
+    // }).catch(function (error) {
+    //   // Handle Errors here.
+    //   var errorCode = error.code;
+    //   var errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   var email = error.email;
+    //   // The firebase.auth.AuthCredential type that was used.
+    //   var credential = error.credential;
+    //   // ...
+    // });
+
+    firebase.auth().signInWithRedirect(provider).then(function() {
+      return firebase.auth().getRedirectResult();
+    }).then(function(result) {
+      // This gives you a Google Access Token.
+      // You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
       // ...
-    }).catch(function (error) {
+    }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
     });
   });
 
