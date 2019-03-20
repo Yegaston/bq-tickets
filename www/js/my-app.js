e3d84c181021786c10c262068bec191a7017b7ee
Gaston Graciani <gastongraciani@gmail.com>
2019-03-19 23:47:41 -0300

    Fixed DataBase Error and can show events in main screen

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -66,14 +66,14 @@ var mainView = app.views.create('.view-main');
 // CLASES DECLARATIONS
 // THIS USE FOR THE AUTH WITH MAIL
 emailProvider = new emailProvider;
-console.log('db' + db)
 // var emailProvider = new emailProvider();
+
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function () {
   console.log("Device is ready!");
-  automaticGenerateEvents();
-
+  // emailProvider.automaticGenerateEvents();
+  
 });
 
 // ###########
@@ -115,25 +115,7 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
   $$('#user-email-panel').text(userEmail);
 
 
-  for (var i = 0; i < 3; i++) {
-
-    // Esto genera las card de los posteos.
-    $$('#feed-container').append(`
-    <div class="card demo-card-header-pic">
-      <div style="background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRptiPOrwVa5w_tmxE5lF_S1i9sIrx8ZAQHqBPptzt4Qk01EX0M)"
-        class="card-header align-items-flex-end">Journey To Mountains</div>
-      <div>
-          <!-- Aca va la parte del organizador. -->
-      </div>
-      <div class="card-content card-content-padding">
-        <p class="date">Posted on January 21, 2015</p>
-        <p>Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies efficitur vitae non felis.
-            Phasellus quis nibh hendrerit...</p>
-      </div>
-      <div class="card-footer"><a class="link sheet-open" data-sheet=".my-sheet">Mas info</a><a href="#" class="link">Asistir</a></div>
-    </div>
-  `);
-  }
+  emailProvider.getEventsDataIndexCards();
 
   $$('#mas-info-header').click(function (e) {
     e.preventDefault();
@@ -255,87 +237,3 @@ $$(document).on('page:init', '.page[data-name="main-screen"]', function (e) {
 
 
 })
-
-
-var db = firebase.firestore();
-
-function automaticGenerateEvents() {
-  db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  })
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
-    });
-
-
-  // db.collection("eventos").add({
-  //     title: 'No Te Va Gustar',
-  //     organizador: 'No Te Va Gustar',
-  //     descrip: "Presentan su nuevo disco.",
-  //     foto: 'https://www.diariocol.com/wp-content/uploads/2017/09/notevag.jpg',
-  // //    tags: ['rock', 'musica', 'rosario'],
-  //     lugar: 'Salon Metropolitano',
-  //     Dia: '21/4/2019',
-  //     hora: '23:00',
-  //     asistentes: 0,
-  //  //   tipoDeEntrada: ['general S/A'],
-  //  //   boleterias: ['https://www.ticketek.com.ar/no-te-va-gustar/teatro-gran-rex', 'www.notevagustar.com']
-
-  // })
-  // .then(function(docRef) {
-  //     console.log("Document written with ID: ", docRef.id);
-  // })
-  // .catch(function(error) {
-  //     console.error("Error adding document: ", error);
-  // });
-
-  // db.collection("eventos").add({
-  //     title: 'Aladin, una aventura magica',
-  //     organizador: '@aladinelshow',
-  //     descrip: 'Un evento magico para disfrutar en familia', 
-  //     foto: 'http://am1300lasalada.com.ar/wp-content/uploads/2018/07/aladin-portada.jpg',
-  //     tags: ['niños', 'teatro', 'rosario'],
-  //     lugar: 'Teatro Broadway',
-  //     Dia: '12/6/2019',
-  //     hora: '15:30',
-  //     asistentes: 0,
-  //     tipoDeEntrada: ['Fila 1/3 Numeradas', 'Fila 4/6 Numeradas', 'Tertulia ', 'Palcos Izq y Der'],
-  //     boleterias: ['https://www.ticketek.com.ar/aladin/teatro-gran-rex']
-
-  // })
-  // .then(function(docRef) {
-  //     console.log("Document written with ID: ", docRef.id);
-  // })
-  // .catch(function(error) {
-  //     console.error("Error adding document: ", error);
-  // });
-
-  // db.collection("eventos").add({
-  //     title: 'Rosario Central - Gremio',
-  //     organizador: 'Copa Libertadores',
-  //     descrip: 'Partido por la copa mas importante de america!.', 
-  //     foto: 'https://futbolete.com/wp-content/uploads/2016/05/rosario-central-gremio-copa-libertadores.jpg',
-  //     tags: ['deportes', 'futbol', 'rosario', 'copa'],
-  //     lugar: 'Estadio de Rosario Central',
-  //     Dia: '16/3/2019',
-  //     hora: '21:30',
-  //     asistentes: 0,
-  //     tipoDeEntrada: ['Fila 1/3 Numeradas', 'Fila 4/6 Numeradas', 'Tertulia ', 'Palcos Izq y Der'],
-  //     boleterias: ['https://www.ticketek.com.ar/aladin/teatro-gran-rex']
-
-  // })
-  // .then(function(docRef) {
-  //     console.log("Document written with ID: ", docRef.id);
-  // })
-  // .catch(function(error) {
-  //     console.error("Error adding document: ", error);
-  // });
-
-
-
-}
